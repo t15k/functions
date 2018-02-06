b2243711dd5596b32a8601f57a1510d6322d4e94
Tonny Staunsbrink <tonny.staunsbrink@skyscanner.net>
2018-02-06 12:04:56 +0000

    Push branch to end of path.
    Some color updates.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,10 +1,15 @@
 function fish_prompt
-    set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
-    echo -n (date "+%H:%M ")
-    set_color 777
-    echo [(pwd)[-1]]
-    set_color normal
-    echo -n [aws:$AWS_ACCESS_KEY_ID]
-    echo [$git_branch] '>'
+  set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+  echo -n [aws:$AWS_ACCESSKEY_ID]
+  echo -n (date "+%H:%M")
+  set_color 0A0
+  echo -n (pwd)
+  if [ "" != "$git_branch" ]
+    set_color 0C0
+    echo -n "#$git_branch"
+    set_color 0A0
+  end
+  set_color 0E0
+  echo "->"
 end
 
