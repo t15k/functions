b4017ab63b7368067ab95b369d39ac64bae8b745
Tonny Staunsbrink <tonny.staunsbrink@skyscanner.net>
2019-01-01 12:30:57 +0000

    Updated promp.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,27 @@
-function fish_prompt
-  set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+function fish_prompt --description "write the prompt"
+  # Values
+  set -l Date (date "+%H:%M")
+  set -l Prompt (basename (pwd))
+  set -l GitBranch (git branch ^/dev/null | sed -n '/\* /s///p')
+  # Colors
+  set -l BaseColor 222
+  set -l PathColor 007
+  set -l BranchColor 0D0
+  set_color -b $BaseColor
+
+  printf "$Date"
   echo -n [aws:$AWS_ACCESSKEY_ID]
-  echo -n (date "+%H:%M")
-  set_color 0A0
-  echo -n (pwd)
-  if [ "" != "$git_branch" ]
-    set_color 0C0
-    echo -n "#$git_branch"
-    set_color 0A0
+  set_color -b $PathColor
+  echo -n " $Prompt"
+  #set_color -b 070
+  if [ "" != "$GitBranch" ]
+    set_color $BranchColor
+    echo -n "#$GitBranch"
+    #set_color -b 070
   end
-  set_color 0E0
-  echo "->"
+  set_color -b normal
+  # \u25B6 
+  printf "\uFF1E"
+  #echo '->\u25B6'
 end
 
