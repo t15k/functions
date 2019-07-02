a15a460013c055a618d3f8e684004205773fd548
Tonny Staunsbrink <tonny.staunsbrink@skyscanner.net>
2019-07-02 14:21:50 +0100

    Fix prompt.
    And simplify.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,39 +1,22 @@
 function fish_prompt
   # Values
   if set -q VIRTUAL_ENV
-    echo -n -s (set_color -b 333 blue) "(" (basename "$VIRTUAL_ENV") ") " (set_color normal)
+    echo -n -s (set_color -b 333 blue) (basename "$VIRTUAL_ENV") " " (set_color normal)
   end
-#  set -l Date (date "+%H:%M")
   set -l Prompt (basename (pwd))
   set -l GitBranch (git branch ^/dev/null | sed -n '/\* /s///p')
 
   # Colors
   set -l BaseColor 222
   set -l PathColor 007
-  set -l BranchColor 0D0
+  set -l BranchColor yellow
 
   set_color -b $BaseColor
-  printf "$Date"
-#  printf " ["
-#  if [ "" != "$AWS_ACCESS_KEY_ID" ]
-##    set_color e47911
- #   printf "+aws"
- #   set_color normal
- # end
- # printf "] "
-  #set_color -b $PathColor
-  echo -n -s (set_color -b 333 brgreen) (basename (pwd))
-  #"$Prompt"
-  #set_color -b 070
+  echo -n -s (set_color -b 333 green) (basename (pwd))
   if [ "" != "$GitBranch" ]
-    #set_color $BranchColor
-    echo -n -s (set_color -b 444 yellow) '#' $GitBranch (set_color normal)
-    #echo -n "#$GitBranch"
-    #set_color -b 070
+    echo -n -s (set_color -b 444 $BranchColor) '#' $GitBranch (set_color normal)
   end
   set_color -b normal
-  # \u25B6 
   printf "\uFF1E"
-  #echo '->\u25B6'
 end
 
